b3b8a29cd1b8c03b3d0a207ab30cf40ffe9f13a1
tarajeanne <t.jeanne.patel@gmail.com>
2019-10-11 10:54:37 -0500

    updated screenshots, added third project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,7 +19,7 @@ class Projects extends React.Component {
       expandedDesc:
         'Upon login, users can see a list of words they are practicing. When the user clicks the practice button, they are shown a word chosen based on a spaced repetition algorithm. The algorithm is implemented in the server by manipulating a singly linked list in the database. Upon a correct answer, that word moves back in the list many spaces, depending on how often the user gets it right. If the user gets it wrong, it moves back only one space, and the user sees it sooner.',
       name: '¡Aprende!',
-      tech: 'JavaScript, CSS3, React, React Router, Node.js, Express, Mocha, Chai, Enzyme, PostgreSQL, JSON Web Tokens, bcrypt',
+      tech: 'JavaScript, CSS3, React, React Router, Node.js, Express, Mocha, Chai, Cypress, PostgreSQL, JSON Web Tokens, bcrypt',
       server:'https://github.com/thinkful-ei-emu/Zane-Tara-Spaceful-Repetition-Client',
       client: 'https://github.com/thinkful-ei-emu/Zane-Tara-Spaceful-Repetition-Client',
       live: 'https://aprende.tara-zane.now.sh/login'
@@ -27,13 +27,13 @@ class Projects extends React.Component {
 
     const DnD = {
       desktop1:
-        'https://github.com/tarajeanne/image_repo/blob/master/desktop_printscreen.png?raw=true',
+        'https://raw.githubusercontent.com/tarajeanne/image_repo/master/dnd-desktop-1.png',
       desktop1Alt: 'View of character creator home screen',
-      desktop2: 'https://github.com/tarajeanne/image_repo/blob/master/destop_homepage.png?raw=true',
+      desktop2: 'https://raw.githubusercontent.com/tarajeanne/image_repo/master/dnd-desktop-2.png',
       desktop2Alt: 'view of character creator character sheet print screen',
-      mobile1:'https://github.com/tarajeanne/image_repo/blob/master/phone_backgroundscreen.png?raw=true',
-      mobile1Alt:'view of character creator background selection screen',
-      mobile2: 'https://github.com/tarajeanne/image_repo/blob/master/phone_statscreen.png?raw=true',
+      mobile1:'https://raw.githubusercontent.com/tarajeanne/image_repo/master/dnd-mobile-1.png',
+      mobile1Alt:'view of character creator class selection screen',
+      mobile2: 'https://raw.githubusercontent.com/tarajeanne/image_repo/master/dnd-mobile-2.png',
       mobile2Alt: 'view of character creator stats generation screen',
       desc:
         'An application that walks users through the steps of creating a character for Dungeons and Dragons.',
@@ -45,12 +45,34 @@ class Projects extends React.Component {
       client: 'https://github.com/tarajeanne/dnd-client',
       live: 'https://dnd-creator.tjeannepatel.now.sh/'
     };
+
+    const symptomTracker = {
+      desktop1:
+        'https://raw.githubusercontent.com/tarajeanne/image_repo/master/symptom-desktop-1.png',
+      desktop1Alt: 'View of user dashboard',
+      desktop2: 'https://raw.githubusercontent.com/tarajeanne/image_repo/master/symptom-desktop-2.png',
+      desktop2Alt: 'View of add symptom screen',
+      mobile1:'https://raw.githubusercontent.com/tarajeanne/image_repo/master/symptom-mobile-1.png',
+      mobile1Alt:'View of add meal on mobile',
+      mobile2: 'https://raw.githubusercontent.com/tarajeanne/image_repo/master/symptom-mobile-2.png',
+      mobile2Alt: 'View of dashboard on mobile',
+      desc:
+        'An application that walks users through the steps of creating a character for Dungeons and Dragons.',
+      expandedDesc:
+        'Upon login, users can see a list of their characters and create new ones. Once created, the app walks the user through the basic parts of creating a character. The stats screen updates live based on user selections, and informs the user how they got certain options. Finally, the app populates vital information in a character sheet that can be printed directly from the browser. This app will expand slowly as I find time to convert the Dungeons and Dragons core rulebook into JavaScript data structures.',
+      name: 'Dungeons and Dragons Character Creator',
+      tech: 'JavaScript, CSS3, React, React Router, Node.js, Express, Mocha, Chai, Enzyme, PostgreSQL, JSON Web Tokens, bcrypt',
+      server:'https://github.com/tarajeanne/dnd-server',
+      client: 'https://github.com/tarajeanne/dnd-client',
+      live: 'https://dnd-creator.tjeannepatel.now.sh/'
+    }
     return (
       <section id="projects">
         <header class="section-header">
           <h2>My projects</h2>
         </header>
         <div class="section-content project-content">
+          <ProjectTile data={symptomTracker} />
           <ProjectTile data={DnD} />
           <ProjectTile data={spacedRepetition} />
         </div>
